0a5ec2a83f5c92d28920e61fb735afe3d23083e1
serapXXXD <serapXXXD@gmail.com>
2023-05-02 16:35:53 +0300

    add tags

--- new/blog/views.py
+++ new/blog/views.py
@@ -1,3 +1,7 @@
+
+from django.db.models.query import QuerySet
+from django.http import HttpResponse
+from django.shortcuts import render
 from .models import *
 from django.db.models import Q
 
@@ -9,9 +13,9 @@ class IndexSearchView(ListView):
     template_name = 'index.html'
     paginate_by = 5
 
-
     def get_queryset(self):
         query = self.request.GET.get('search')
+
         if query and query != '@#$%^&*()_+=':
             object_list = Post.objects.filter(
                 Q(body__icontains=query) | Q(title__icontains=query))
@@ -23,3 +27,11 @@ class IndexSearchView(ListView):
         context = super().get_context_data(object_list=None, **kwargs)
         context['tags'] = Tag.objects.all()
         return context
+
+
+def tag_view(request, tag_id):
+    object_list = Post.objects.filter(tags=tag_id)
+    tags = Tag.objects.all()
+    context = {'tags': tags,
+               'object_list': object_list,}
+    return render(request, 'tag.html', context)
